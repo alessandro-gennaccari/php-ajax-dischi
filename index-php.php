cc3370b6c36ec840761f388453b6dd8e0723fb23
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-03-01 14:27:13 +0100

    added header & cicles

--- index-php.php
+++ index-php.php
@@ -13,6 +13,11 @@
 </head>
 <body>
 
+    <header>
+        <div class="container">
+            <img class="margin-top-15" src="img/spotify-logo.png" alt="Logo Spotify">
+        </div>
+    </header>
     
 </body>
 </html>
